dd65f2f5f3bb6f05638ca0cbfec57f7aed6580eb
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2021-01-15 23:14:08 -0300

    roolback api

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+// const api = axios.create({
+//   baseURL: `${process.env && process.env === 'development' ? process.env.REACT_APP_LINK_API : process.env.REACT_APP_LINK_API_PROD}`,
+// });
+
 const api = axios.create({
-  baseURL: `${process.env && process.env === 'development' ? process.env.REACT_APP_LINK_API : process.env.REACT_APP_LINK_API_PROD}`,
+  baseURL: 'https://bravosul-app.herokuapp.com',
 });
 
 export default api;
