608792ade114be8e62a60c7c5eb09b5b2172a847
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2021-01-29 15:20:43 -0300

    base url prod

--- src/Services/api.js
+++ src/Services/api.js
@@ -5,8 +5,8 @@ import axios from 'axios';
 // });
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
-  // baseURL: 'http://development.nodejs202.uni5.net:21007',
+  // baseURL: 'http://localhost:3333',
+  baseURL: 'http://development.nodejs202.uni5.net:21007',
 });
 
 export default api;
